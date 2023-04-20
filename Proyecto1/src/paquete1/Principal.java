790b2a142541855780b653bc153c8e2243a1323d
DAM1 <DAM1@DESKTOP-3LHBG1B>
2023-04-20 20:17:06 +0200

    k

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Estamos haciendo control de versiones con Git");
 		System.out.println("Hola nuevo");
+		System.out.println("Hola de neuv");
 	}
 
 }
